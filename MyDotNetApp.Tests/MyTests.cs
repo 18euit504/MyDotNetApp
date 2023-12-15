c4733801f80e85d531c13adc6fa88bdd8432ac5a
18euit504 <60766636+18euit504@users.noreply.github.com>
2023-12-15 14:09:26 +0530

    Create MyTests.cs

---
+++ MyDotNetApp.Tests/MyTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MyDotNetApp.Tests
+{
+    [TestFixture]
+    public class MyTests
+    {
+        [Test]
+        public void TestHelloWorld()
+        {
+            Assert.Pass();
+        }
+    }
+}
